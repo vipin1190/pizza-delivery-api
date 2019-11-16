8e21d7ae5b8804a9519de99bed49b4a126f20a62
vipin suthar <vipin.jangid45@gmail.com>
2019-11-16 16:54:27 +0530

    * Define initial file structure planning for Pizza Delivery API.

---
+++ index.js
@@ -0,0 +1,10 @@
+/** 
+ * Pizza Delivery API
+ * @author
+ *  vipin suthar
+ * @description
+ *  Pizza Delivery API is a nodeJS ReSTful application provides services for
+ *  most basic and common operational processes.
+ */
+
+console.log('Hello World!');
